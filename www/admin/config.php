89861e8a267e64863e0cbd3bd268b2376b742752
D-drop <goodbye.say@bk.ru>
2014-02-27 01:23:05 +0400

    опенкарт

---
+++ www/admin/config.php
@@ -0,0 +1,32 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://www.opencart.loc/admin/');
+define('HTTP_CATALOG', 'http://www.opencart.loc/');
+define('HTTP_IMAGE', 'http://www.opencart.loc/image/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://www.opencart.loc/admin/');
+define('HTTPS_CATALOG', 'http://www.opencart.loc/');
+define('HTTPS_IMAGE', 'http://www.opencart.loc/image/');
+
+// DIR
+define('DIR_APPLICATION', 'Z:\home\opencart.loc\www/admin/');
+define('DIR_SYSTEM', 'Z:\home\opencart.loc\www/system/');
+define('DIR_DATABASE', 'Z:\home\opencart.loc\www/system/database/');
+define('DIR_LANGUAGE', 'Z:\home\opencart.loc\www/admin/language/');
+define('DIR_TEMPLATE', 'Z:\home\opencart.loc\www/admin/view/template/');
+define('DIR_CONFIG', 'Z:\home\opencart.loc\www/system/config/');
+define('DIR_IMAGE', 'Z:\home\opencart.loc\www/image/');
+define('DIR_CACHE', 'Z:\home\opencart.loc\www/system/cache/');
+define('DIR_DOWNLOAD', 'Z:\home\opencart.loc\www/download/');
+define('DIR_LOGS', 'Z:\home\opencart.loc\www/system/logs/');
+define('DIR_CATALOG', 'Z:\home\opencart.loc\www/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart');
+define('DB_PREFIX', '');
+?>
---
+++ www/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://www.opencart.loc/');
+define('HTTP_IMAGE', 'http://www.opencart.loc/image/');
+define('HTTP_ADMIN', 'http://www.opencart.loc/admin/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://www.opencart.loc/');
+define('HTTPS_IMAGE', 'http://www.opencart.loc/image/');
+
+// DIR
+define('DIR_APPLICATION', 'Z:\home\opencart.loc\www/catalog/');
+define('DIR_SYSTEM', 'Z:\home\opencart.loc\www/system/');
+define('DIR_DATABASE', 'Z:\home\opencart.loc\www/system/database/');
+define('DIR_LANGUAGE', 'Z:\home\opencart.loc\www/catalog/language/');
+define('DIR_TEMPLATE', 'Z:\home\opencart.loc\www/catalog/view/theme/');
+define('DIR_CONFIG', 'Z:\home\opencart.loc\www/system/config/');
+define('DIR_IMAGE', 'Z:\home\opencart.loc\www/image/');
+define('DIR_CACHE', 'Z:\home\opencart.loc\www/system/cache/');
+define('DIR_DOWNLOAD', 'Z:\home\opencart.loc\www/download/');
+define('DIR_LOGS', 'Z:\home\opencart.loc\www/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart');
+define('DB_PREFIX', '');
+?>
